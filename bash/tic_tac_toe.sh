303c99d3865afd03e941fd4bfd154646d62226af
Aleksandra Grosiak <aleks4ndr.grosiak77@gmail.com>
2025-05-19 13:00:51 +0200

    Bash: 4.0 Save game

--- bash/tic_tac_toe.sh
+++ bash/tic_tac_toe.sh
@@ -1,14 +1,13 @@
 #!/bin/bash
-
-
-
-
+save_file="save.txt"
 chosen_field=" "
-board=( 1 2 3 4 5 6 7 8 9 )
+board=( 1 2 3 4 5 6 7 8 9 1) #board[9] is a round
 active_player=1
-round=1
 sign="X"
 
+saving_game(){
+    echo "${board[*]}" > "$save_file"
+}
 
 printing_board(){
     echo "${board[0]} | ${board[1]} | ${board[2]}"
@@ -19,64 +18,36 @@ printing_board(){
 }
 
 check_if_win(){
-    
-    if [ "${board[0]}" == "${board[1]}" ] && [ "${board[1]}" == "${board[2]}" ]; then
+    if [[ "${board[0]}" == "${board[1]}" && "${board[1]}" == "${board[2]}" ]] ||
+       [[ "${board[3]}" == "${board[4]}" && "${board[4]}" == "${board[5]}" ]] ||
+       [[ "${board[6]}" == "${board[7]}" && "${board[7]}" == "${board[8]}" ]] ||
+       [[ "${board[0]}" == "${board[3]}" && "${board[3]}" == "${board[6]}" ]] ||
+       [[ "${board[1]}" == "${board[4]}" && "${board[4]}" == "${board[7]}" ]] ||
+       [[ "${board[2]}" == "${board[5]}" && "${board[5]}" == "${board[8]}" ]] ||
+       [[ "${board[0]}" == "${board[4]}" && "${board[4]}" == "${board[8]}" ]] ||
+       [[ "${board[2]}" == "${board[4]}" && "${board[4]}" == "${board[6]}" ]]; then
         printing_board
-        echo "Winner player $active_player"
-	sleep 5
-        exit 0
-    elif [ "${board[3]}" == "${board[4]}" ] && [ "${board[4]}" == "${board[5]}" ]; then
-        printing_board
-        echo "Winner player $active_player"
-	sleep 5
-        exit 0
-    elif [ "${board[6]}" == "${board[7]}" ] && [ "${board[7]}" == "${board[8]}" ]; then
-        printing_board
-        echo "Winner player $active_player"
-	sleep 5
-        exit 0
+        echo "Winner: Player $active_player!"
+        rm -f "$save_file"
+		sleep 5
+		exit 0
     fi
-    if [ "${board[0]}" == "${board[3]}" ] && [ "${board[3]}" == "${board[6]}" ]; then
-        printing_board
-        echo "Winner player $active_player"
-	sleep 5
-        exit 0
-    elif [ "${board[1]}" == "${board[4]}" ] && [ "${board[4]}" == "${board[7]}" ]; then
-        printing_board
-        echo "Winner player $active_player"
-	sleep 5
-        exit 0
-    elif [ "${board[2]}" == "${board[5]}" ] && [ "${board[5]}" == "${board[8]}" ]; then
-        printing_board
-        echo "Winner player $active_player"
-	sleep 5
-        exit 0
-    fi
-    if [ "${board[0]}" == "${board[4]}" ] && [ "${board[4]}" == "${board[8]}" ]; then
-        printing_board
-        echo "Winner player $active_player"
-	sleep 5
-        exit 0
-    elif [ "${board[2]}" == "${board[4]}" ] && [ "${board[4]}" == "${board[6]}" ]; then
-        printing_board
-        echo "Winner player $active_player"
-	sleep 5
-        exit 0
-    fi
-    if (( round > 9 )); then
-        printing_board
+    if (( board[9] > 9 )); then
         echo "It's a tie!"
-	sleep 5
+		printing_board
+        rm -f "$save_file"
+		sleep 5
         exit 0
     fi
 }
 
 switching_fields(){
     read chosen_field
-    if [[ ${board[$((chosen_field - 1))]} != "X" && ${board[$((chosen_field - 1))]} != "O" ]]; then
+    if [[ ${board[$((chosen_field - 1))]} != "X" && ${board[$((chosen_field - 1))]} != "O" && "$chosen_field" != "10" ]]; then
         board[$((chosen_field - 1))]=$sign
-        ((round++))
+        ((board[9]++))
         check_if_win
+        saving_game
     else
         echo "Invalid field"
     fi
@@ -84,9 +55,10 @@ switching_fields(){
 }
 
 play_round(){
-    echo "Round $round"
+    echo "Round ${board[9]}"
     printing_board
-    if (( round % 2 == 1 )); then
+
+    if (( board[9] % 2 == 1 )); then
         sign="X"
         active_player=1
         echo "Player 1 choose the field"
@@ -94,12 +66,39 @@ play_round(){
         sign="O"
         echo "Player 2 choose the field"
         active_player=2
-        fi
-        switching_fields
+    fi
+       switching_fields
     }
 
 starting_game(){
-    options
+    if [[ -f "$save_file" ]]; then
+        echo "It seems you already started playing, you want to continue?"
+        echo "1) yes"
+        echo "2) no"
+        read -r option
+    
+        case $option in 
+            1)
+			
+            IFS=' '
+            read -r -a board -d EOF< "$save_file"
+			board[9]=$(echo "${board[9]}" | tr -d '\n')
+            play_round
+            ;;
+        
+            2)
+            rm -f "$save_file"
+            options
+            ;;
+        
+            *)
+            echo  "Wrong answer"
+            starting_game
+            ;;
+        esac
+    else
+		options
+    fi
 }
 
 
@@ -131,5 +130,5 @@ options(){
 }
 
 
-echo "Welcome to tic-tac-toe."
+echo "Welcome to tic-tac-toe. The game is saving your progress after every round."
 starting_game
