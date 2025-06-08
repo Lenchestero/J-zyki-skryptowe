09e3883927f900b393e575c438aa7d49f412cc27
Aleksandra Grosiak <aleks4ndr.grosiak77@gmail.com>
2025-06-08 22:07:03 +0200

    JS: 5.0 Level generator

--- mario/src/scenes/Level1.js
+++ mario/src/scenes/Level1.js
@@ -15,6 +15,10 @@ export class Level1 extends Phaser.Scene {
         this.slimes = null;  
     }
 
+    init(data){
+        this.isRandom = data.isRandom
+    }
+
     preload(){
         this.load.image('background', 'assets/background.png');
         this.load.image('tile2', 'assets/Tile_02.png');
@@ -31,37 +35,43 @@ export class Level1 extends Phaser.Scene {
 
     add_platform(ismovable, length, height, position, isEnemy = false){
         if(!ismovable){
-            for(let x= 0 ;  x <= length; x++ ){
-                let h =position + (x * 32) 
-                this.platforms.create(h,height,'tile2').setScale(2).refreshBody();
+            for(let x= 0 ;  x < length; x++ ){
+                let h =position + (x * 64) 
+                this.platforms.create(h,height,'tile2').setScale(2).setOrigin(0).refreshBody();
             }
             if(isEnemy){
                 const slime = new Enemy(this,position + length/2 *32 , height-80).setScale(2);
                 slime.make_Animations();
-                slime.setData("MinX", position - 70 );
-                slime.setData("MaxX", position + length *32 - 30);
+                slime.setData("MinX", position - 25);
+                slime.setData("MaxX", position + length * 64 - 75);
                 this.slimes.add(slime)
             }
         }
         else{
-            const new_platform = this.moving_platforms.create(position, height, 'tile_move').setScale(2).setOrigin(0.5, 0.5);
+            const new_platform = this.moving_platforms.create(position, height, 'tile_move').setScale(2).setOrigin(0);
             new_platform.setImmovable(true);
             new_platform.body.setAllowGravity(false); 
             new_platform.setVelocityX(150)
             new_platform.setData("platformMinX", position - 50);
-            new_platform.setData("platformMaxX", position + 600);   
+            new_platform.setData("platformMaxX", position + 400);   
         }
-        for (let x= 0; x <= Math.floor(length/2); x++ ) {
-            let h =position + (x * 32) + (x*30)
-            let coin = this.coin.create(h, height -100, 'coin').setScale(2).refreshBody();
+        for (let x= 0; x < length; x++ ) {
+            let h =position + (x * 64)
+            let coin = this.coin.create(h + 16, height - 50, 'coin').setScale(2).setOrigin(0).refreshBody();
             coin.body.allowGravity = false;
             coin.setImmovable(true);
             this.coins_spawned++;
         }
     }
 
-    loading(level){
-        const level_content = this.cache.json.get(level)
+    loading(level, isRandom = false){
+        let level_content;
+        if(!isRandom){
+            level_content = this.cache.json.get(level)
+        }
+        else{
+            level_content = level
+        }
         this.platforms = this.physics.add.staticGroup();
         this.moving_platforms = this.physics.add.group()
         this.coin= this.physics.add.group();
@@ -73,7 +83,7 @@ export class Level1 extends Phaser.Scene {
         level_content.platforms.forEach(p =>{
             this.add_platform(p.ismovable, p.length, p.height, p.position, p.isEnemy)
             if(dude_spawn && !p.ismovable){
-                this.dude= new Dude(this,p.position + (Math.floor(p.length/2) * 32),p.height - 100);
+                this.dude= new Dude(this,p.position + (Math.floor(p.length/2) * 64),p.height - 100);
                 this.position_x = this.dude.body.position.x;
                 this.position_y = this.dude.body.position.y;
                 this.dude.make_Animations();
@@ -88,10 +98,71 @@ export class Level1 extends Phaser.Scene {
         }
     }
 
+
+    randomize(){
+        const random_level = {
+            platforms: []
+        }
+        const max_height = this.scale.height - 64
+        const max_width = Math.floor(this.scale.width, 64)
+        let platform_count = Phaser.Math.Between(8,14)
+        let enemy_count = Phaser.Math.Between(2,platform_count/2)
+        let movable_platform_count = Phaser.Math.Between(1,3)
+        let dude_spawn = false;
+        let p_c = platform_count + movable_platform_count
+        for(let i=0; i< p_c; i++){
+            let height = Math.max(max_height - (Phaser.Math.Between(0,6) * 174), 134);
+            if(platform_count > 0){
+                let length = Phaser.Math.Between(1,4);
+                let position = Phaser.Math.Between(0, max_width - length * 64);
+                while (true) {
+                    let check = 0
+                    for(let x of random_level.platforms){
+                        let rectangle = new Phaser.Geom.Rectangle(position, height, length * 64, 64)
+                        let rectangle_x = new Phaser.Geom.Rectangle(x.position, x.height, x.length * 64, 64)
+                        let z = Phaser.Geom.Intersects.GetRectangleToRectangle(rectangle, rectangle_x)
+                        if(z.length > 0){
+                            check = 1
+                            length = Phaser.Math.Between(1,4);
+                            position = Phaser.Math.Between(0, max_width - length * 64);
+                            height = Math.max(max_height - (Phaser.Math.Between(0,6) * 174), 134);
+                        }
+                    }
+                    if (check == 0){
+                        break;
+                    }
+                    check = 0;
+                }
+                
+                let isEnemy;
+                if(enemy_count > 0 && dude_spawn){
+                    isEnemy = true;
+                    enemy_count -= 1;
+                }
+                dude_spawn = true
+                platform_count -= 1;
+                random_level.platforms.push({ismovable: false, length: length, height: height, position: position, isEnemy: isEnemy})
+            }
+            else if(movable_platform_count > 0){
+                let position = Phaser.Math.Between(0, max_width - 656);
+                movable_platform_count -= 1;
+                random_level.platforms.push({ismovable: true, length: 5, height: height, position: position, isEnemy: false})
+            }
+        }
+        
+        this.loading(random_level, true)
+    }
+
     create() {
         this.physics.world.setBounds(0, -700, 1280, 1700);
         this.add.image(640,360,'background');
-        this.loading("level");
+        if (!this.isRandom){
+            this.loading("level");
+        }
+        else{
+            this.randomize();
+        }
+       
         this.scoreText = this.add.text(16, 16, 'Score: 0', {
             fontSize: '24px',
             fill: '#ffffff'
@@ -163,7 +234,7 @@ export class Level1 extends Phaser.Scene {
         if(this.slimes && this.slimes.children){
             this.slimes.children.iterate((slime)=>{if (slime) {slime.move(slime.body.position.x, slime.getData("MinX"),slime.getData("MaxX"))}})
         }
-        if ((this.dude.y > 620)|| (this.dude.health == 0)) {
+        if ((this.dude.y > 720)|| (this.dude.health == 0)) {
             if(this.dude.lifes == 1){
                 this.coins_collected = 0;
                 this.scene.start('Death');
--- mario/src/scenes/Menu.js
+++ mario/src/scenes/Menu.js
@@ -18,12 +18,19 @@ export class Menu extends Phaser.Scene {
         const subtitle= this.add.text(450, 300, 'Press ENTER to start game', { fontSize: '24px', fill: '#fff', strokeThickness: 1, stroke: '#fff' });
         subtitle.setOrigin(0.5);
         subtitle.setPosition(this.cameras.main.centerX, this.cameras.main.centerY+50);
+        const subtitle2= this.add.text(450, 300, 'Press SPACE to enter random level', { fontSize: '24px', fill: '#fff', strokeThickness: 1, stroke: '#fff' });
+        subtitle2.setOrigin(0.5);
+        subtitle2.setPosition(this.cameras.main.centerX, this.cameras.main.centerY+100);
         this.enterKey = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.ENTER);
+        this.spaceKey = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.SPACE);
     }
 
     update(time){
         if(this.enterKey.isDown){
-            this.scene.start('Level1');
+            this.scene.start('Level1',{isRandom: false});
+        }
+        if(this.spaceKey.isDown){
+            this.scene.start('Level1',{isRandom: true});
         }
     }
 }
