a3d2be58d3c8efce9ce445efdd7dbfcf6b574be8
Aleksandra Grosiak <aleks4ndr.grosiak77@gmail.com>
2025-06-16 18:57:23 +0200

    Python: 3.0 E-sport bot with 3 prompt chains

---
+++ python/main.py
@@ -0,0 +1,56 @@
+import ollama
+import discord
+from discord import app_commands
+
+LLM_MODEL = "gemma3:12b"
+DISCORD_TOKEN = ""
+
+intents = discord.Intents.default()
+client = discord.Client(intents=intents)
+command_tree = app_commands.CommandTree(client)
+
+teams_data = [{"team": "A", "players": "Adolf, Agnieszka, Albert, Hipacy, Izaur"},
+{"team": "B", "players": "Awit, Awita, Drogomysł, Izaura, Marcjan"},
+{"team": "C", "players": "Franciszek, Gundolf, Herweusz, Nikander, Nikandra"}]
+
+tournament_name = ""
+
+def ask_llm(prompt):
+    response = ollama.chat(model=LLM_MODEL, messages=[{"role": "user", "content": prompt}])
+    response = response["message"]["content"]
+    return response
+
+@command_tree.command(name="introduction", description="Introduction to the tournament")
+async def introduction(interaction: discord.Interaction, game: str):
+    await interaction.response.defer(thinking=True)
+    tournament_name = ask_llm("Create some fancy name for e-sport tournament. Output only this name")
+    prompt = (
+        f"Give introduction to the {game} e-sport tournament named {tournament_name}. Come up with a reward. Invite to participate. Don't make it long."
+    )
+    result = ask_llm(prompt)
+    await interaction.followup.send(result[:2000])
+
+@command_tree.command(name="score_system_in_team", description="Generate ranking of players in team")
+async def scoring_players(interaction: discord.Interaction, team: str):
+    await interaction.response.defer(thinking=True)
+    prompt = (
+        f"Here are the data of current teams:{teams_data}. Generate ranking of players on the team {team}. Come up with their k/d/a statistics from past tournaments. Output only the ranking with statistics, don't add other explanation."
+    )
+    result = ask_llm(prompt)
+    await interaction.followup.send(result[:2000])
+
+@command_tree.command(name="score_system", description="Generate ranking of teams")
+async def scoring_teams(interaction: discord.Interaction):
+    await interaction.response.defer(thinking=True)
+    prompt = (
+        f"Here are the data of current teams: {teams_data}. Come up with how many wins they had in past tournaments and their ranking based on it. Output only the ranking with wins, don't add other explanation."
+    )
+    result = ask_llm(prompt)
+    await interaction.followup.send(result[:2000])
+
+@client.event
+async def on_ready():
+    await command_tree.sync()
+    print("Bot connected successfully")
+
+await client.start(DISCORD_TOKEN)
